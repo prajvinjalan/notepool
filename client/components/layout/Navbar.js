1c161bfd04f420fdc37063352973d203874c9fb2
Prajvin Jalan <prajvinjalan@gmail.com>
2017-10-18 23:33:32 -0400

    remove error code

--- client/components/layout/Navbar.js
+++ client/components/layout/Navbar.js
@@ -9,7 +9,6 @@ class Navbar extends Component {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/notes">Notes</Link></li>
           <li><Link to="/users">Users</Link></li>
-          <li><Link onClick={this.props.history.goBack}></Link></li>
         </ul>
       </div>
     )
