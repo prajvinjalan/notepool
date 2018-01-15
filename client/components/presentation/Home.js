879fc98ebae905ab02ebd89193c20b9ee4b0d413
Prajvin Jalan <prajvinjalan@gmail.com>
2018-01-15 14:21:37 -0500

    finish homepage content with some responsiveness fixes

--- client/components/presentation/Home.js
+++ client/components/presentation/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Header, Icon } from 'semantic-ui-react'
 import CustomizeNotes from '../../assets/CustomizeNotes.gif'
+import Devices from '../../assets/Devices.png'
 import RealTimeCollaboration1 from '../../assets/RealTimeCollaboration_1.gif'
 import RealTimeCollaboration2 from '../../assets/RealTimeCollaboration_2.gif'
 import SearchNotes from '../../assets/SearchNotes.gif'
@@ -21,31 +22,58 @@ const Home = (props) => {
         </Header>
       </div>
       <div className='home section image'>
-        <div>
-          <h1>Customize your notes</h1>
+        <div className='text'>
+          <Header size='huge'>
+            Customize your notes
+            <Header.Subheader>
+              Create simple text or list notes and customize them to keep track of the notes you share
+            </Header.Subheader>
+          </Header>
         </div>
         <div>
-          <img id='customizeNotes' src={CustomizeNotes}></img>
+          <img id='customizeNotesGif' src={CustomizeNotes}></img>
         </div>
       </div>
       <div className='home section image'>
-        <div className='left'>
-          <img id='realTimeCollaboration' src={RealTimeCollaboration1}></img>
-          <img id='realTimeCollaboration' src={RealTimeCollaboration2}></img>
+        <div className='double'>
+          <img id='realTimeCollaborationGif1' src={RealTimeCollaboration1}></img>
+          <img id='realTimeCollaborationGif2' src={RealTimeCollaboration2}></img>
+        </div>
+        <div className='text'>
+          <Header size='huge'>
+            Collaborate in real time
+            <Header.Subheader>
+              Invite others to collaborate on your notes as editors or viewers, and watch as updates occur in real time
+            </Header.Subheader>
+          </Header>
+        </div>
+      </div>
+      <div className='home section image'>
+        <div className='text'>
+          <Header size='huge'>
+            Find everything you need
+            <Header.Subheader>
+              Easily search for notes by their content or collaborators, and add filters for attributes like colour and note type.
+            </Header.Subheader>
+          </Header>
         </div>
         <div>
-          <h1>Collaborate in real time</h1>
+          <img id='searchNotesGif' src={SearchNotes}></img>
         </div>
       </div>
       <div className='home section image'>
         <div>
-          <h1>Find everything you need</h1>
+          <img id='devicesImage' src={Devices}></img>
         </div>
-        <div>
-          <img id='searchNotes' src={SearchNotes}></img>
+        <div className='text'>
+          <Header size='huge'>
+            Accessible on all devices
+            <Header.Subheader>
+              All your changes are saved to your account so your notes can be accessed anytime, anywhere
+            </Header.Subheader>
+          </Header>
         </div>
       </div>
-      <div className='home section'>Fourth Box</div>
       <div className='home section'>
         <Header size='small'>
           Try Notepool for free today!
