3e991b3ef3d3dbb35778462999302e72209f3546
Prajvin Jalan <prajvinjalan@gmail.com>
2018-01-18 11:15:21 -0500

    update configuration slightly

--- webpack.config.js
+++ webpack.config.js
@@ -41,5 +41,17 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: false
+      }
+    })
+  ]
 }
